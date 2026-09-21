b0a37d04b3680dcb3b4302402537219b03c5d679
Supsup300 <adrian.salard@gmail.com>
2026-09-21 23:07:40 +0200

    Update AdConfig.java

--- app/src/main/java/fr/adriansalard/monumania/ads/AdConfig.java
+++ app/src/main/java/fr/adriansalard/monumania/ads/AdConfig.java
@@ -1,12 +1,19 @@
 package com.kartel99games.monumania.ads;
 
 public final class AdConfig {
+
     public enum Environment { SIMULATION, PRODUCTION }
 
     public static final Environment ENVIRONMENT = Environment.SIMULATION;
-    public static final String ADMOB_APP_ID = "";
-    public static final String REWARDED_AD_UNIT_ID = "";
-    public static final String INTERSTITIAL_AD_UNIT_ID = "";
+
+    public static final String ADMOB_APP_ID =
+            "ca-app-pub-6035067894119408~1325155490";
+
+    public static final String REWARDED_AD_UNIT_ID =
+            "ca-app-pub-6035067894119408/9012073827";
+
+    public static final String INTERSTITIAL_AD_UNIT_ID =
+            "ca-app-pub-6035067894119408/6385910488";
 
     private AdConfig() {}
 }
